605e80f8f6a5dc1c1a1da6174d473907bd2be61d
oto <mukhulishviliotari@gmail.com>
2026-08-17 17:17:33 +0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,10 @@
 const CACHE_NAME = 'kalkulatori-v1';
 const ASSETS = [
   './',
-  './index.html',
-  './style.css',
-  './kalkulatori.js',
-  './icons8-calculator-100.png'
+  'index.html',
+  'style.css',
+  'kalkulatori.js',
+  'icons8-calculator-100.png'
 ];
 
 self.addEventListener('install', (e) => {
@@ -15,6 +15,9 @@ self.addEventListener('install', (e) => {
 
 self.addEventListener('fetch', (e) => {
   e.respondWith(
-    caches.match(e.request).then((response) => response || fetch(e.request))
+    caches.match(e.request).then((response) => {
+      // Return cached file if found, otherwise attempt fetch
+      return response || fetch(e.request);
+    })
   );
 });
